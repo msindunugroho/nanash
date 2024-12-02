69b56d210f3033e10794f24e6fbadef5664ec436
msindunugroho <m.sindunugroho@gmail.com>
2024-12-02 10:22:45 +0700

    configure tailwindcss and main entry CSS

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import './css/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <h1 className='text-2xl text-slate-700'>test</h1>
   </StrictMode>,
 )
